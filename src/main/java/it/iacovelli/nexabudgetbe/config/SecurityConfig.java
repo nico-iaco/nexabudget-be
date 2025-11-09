4a6fb4d3b274051b29fc3766036e979dee87dac8
Nicola Iacovelli <nicola.iacovelli@soft.it>
2025-11-09 15:30:59 +0100

    Disable CSRF protection in SecurityFilterChain configuration

--- src/main/java/it/iacovelli/nexabudgetbe/config/SecurityConfig.java
+++ src/main/java/it/iacovelli/nexabudgetbe/config/SecurityConfig.java
@@ -81,6 +81,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/**").permitAll()
