c2a9d23cbabe9bafa3403eca637e73c21d8cb64b
nico-iaco <nicolaiacovelli98@gmail.com>
2026-04-29 10:48:23 +0200

    fix: Aggiorna la versione di spring-ai-bom a 2.0.0-M5 e aggiungi il parametro thinkingBudget nel modello di chat

--- src/main/java/it/iacovelli/nexabudgetbe/config/GoogleGenAiConfig.java
+++ src/main/java/it/iacovelli/nexabudgetbe/config/GoogleGenAiConfig.java
@@ -57,6 +57,7 @@ public class GoogleGenAiConfig {
                                 GoogleGenAiChatOptions.builder()
                                                 .model(modelName)
                                                 .temperature(temperature)
+                                                .thinkingBudget(0)
                                                 .build(),
                                 ToolCallingManager.builder()
                                                 .build(),
